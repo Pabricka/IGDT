e11079436e3af5b22169ef1f28e585d258255a18
estula <essi.lahti97@hotmail.com>
2019-11-09 00:41:02 +0200

    added spider enemy

---
+++ Assets/Scripts/SpiderMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += -transform.right * speed * Time.deltaTime;
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("spiderCollider"))
+        {
+            transform.eulerAngles += transform.forward * 90;
+            transform.position += -transform.right * 1;
+        }
+    }
+}
